2fe49bf7c94738af2071a26197fbd7188490e44e
Harry <hsa117@sfu.ca>
2021-03-20 10:19:04 +0800

    Update the home.html Todo: maybe change the detail information date type to easy show in the html and python

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 import random
+
+import sqlalchemy
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -32,16 +34,45 @@ class user(db.Model):
         self.username = username
         self.password = password
         self.id = id
+        self.music_rating_weight = 0
+        self.movie_rating_weight = 0
 
+class movie (db.Model):
+    __tablename__ = 'movie'
+    id = db.Column('id', db.INTEGER(), primary_key=True)
+    title = db.Column('title', db.VARCHAR(255))
+    release_date = db.Column ('release_date', db.VARCHAR(255))
+    country = db.Column ('country', db.VARCHAR(255))
+    detailed_information = ('detailed_information', db.TEXT)
+    def __init__(self,id,title,release_date,country,detailed_information):
+        self.id = id
+        self.title = title
+        self.release_date = release_date
+        self.country = country
+        self.detailed_information = detailed_information
+
+class moviecomment(db.Model):
+    __tablename__ = 'moviecomment'
+    comment_id = db.Column('comment_id', db.INTEGER(), primary_key = True)
+    movie_id = db.Column('movie_id', db.INTEGER(), primary_key = True)
+    createtime = db.Column(db.DATETIME)
+    content = db.Column (db.TEXT)
+
+    def __init__(self,comment_id,movie_id,createtime,content):
+        self.comment_id = comment_id
+        self.movie_id = movie_id
+        self.createtime = createtime
+        self.content = content
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def showmovie():
+    return render_template('home.html', posts=movie.query.all())
 
 
 @app.route('/userinfo')
 def show_all():
-    return render_template('show_all.html', users=user.query.all())
+    a_user = user.query.all()
+    return render_template('show_all.html', users=a_user)
 
 
 @app.route('/reg', methods=['GET', 'POST'])
@@ -53,18 +84,28 @@ def reg():
             print(request.form['username'], request.form['password'], request.form['email'])
             temp_id = random.randint(1, 99999999999)
             users = user.query.all()
+
             isContain = 0
-            while (isContain != 0):
+            while (isContain == 0):
                 for temp in users:
+                    print(temp.id,temp.username,temp.email)
                     if (temp.id == temp_id):
                         temp_id = random.randint(1, 9999999)
                         isContain = 0
                         break
+                    if (temp.username == request.form['username']):
+                        flash('username is exist, please change it.')
+                        return render_template('reg.html')
+                    if (temp.email == request.form['email']):
+                        flash('email is exist, please change it.')
+                        return render_template('reg.html')
+
                 else:
                     isContain = 1
             t_user = user(temp_id, request.form['email'], request.form['username'], request.form['password'])
             db.session.add(t_user)
             db.session.commit()
+            flash('Add user '+request.form['username']+' successfully. ')
             return redirect(url_for('show_all'))
     return render_template('reg.html')
 
