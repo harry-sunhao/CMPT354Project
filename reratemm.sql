02e5014ad4efb3bb0d1e2037bfbfab2c5e55dfa0
Alex Wang <wyl2000@zju.edu.cn>
2021-04-14 19:37:08 +0800

    update ratings and foreign keys in reratemm.sql

--- reratemm.sql
+++ reratemm.sql
@@ -31,7 +31,9 @@ DROP TABLE IF EXISTS `Act`;
 CREATE TABLE IF NOT EXISTS `Act` (
   `movie_id` int NOT NULL,
   `actor_id` int NOT NULL,
-  PRIMARY KEY (`movie_id`,`actor_id`) USING BTREE
+  PRIMARY KEY (`movie_id`,`actor_id`) USING BTREE,
+  INDEX `movie_id_fk` (`movie_id`) USING BTREE,
+  INDEX `actor_id_fk` (`actor_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
@@ -110,7 +112,9 @@ DROP TABLE IF EXISTS `AlbumArtist`;
 CREATE TABLE IF NOT EXISTS `AlbumArtist` (
   `album_id` int NOT NULL,
   `artist_id` int NOT NULL,
-  PRIMARY KEY (`album_id`,`artist_id`) USING BTREE
+  PRIMARY KEY (`album_id`,`artist_id`) USING BTREE,
+  INDEX `album_id_fk` (`album_id`) USING BTREE,
+  INDEX `artist_id_fk` (`artist_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
@@ -137,7 +141,9 @@ CREATE TABLE IF NOT EXISTS `AlbumComment` (
   `createtime` datetime DEFAULT NULL,
   `album_id` int NOT NULL,
   `user_id` int NOT NULL,
-  PRIMARY KEY (`comment_id`) USING BTREE
+  PRIMARY KEY (`comment_id`) USING BTREE,
+  INDEX `album_id_fk` (`album_id`) USING BTREE,
+  INDEX `user_id_fk` (`user_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
@@ -159,24 +165,25 @@ INSERT INTO `AlbumComment` (`comment_id`, `content`, `createtime`, `album_id`, `
 
 DROP TABLE IF EXISTS `AlbumRating`;
 CREATE TABLE IF NOT EXISTS `AlbumRating` (
-  `rate_id` int NOT NULL,
   `createtime` datetime DEFAULT NULL,
   `value` int DEFAULT NULL,
   `album_id` int NOT NULL,
   `user_id` int NOT NULL,
-  PRIMARY KEY (`rate_id`) USING BTREE
+  PRIMARY KEY (`album_id`, `user_id`) USING BTREE,
+  INDEX `album_id_fk` (`album_id`) USING BTREE,
+  INDEX `user_id_fk` (`user_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `AlbumRating`
 --
 
-INSERT INTO `AlbumRating` (`rate_id`, `createtime`, `value`, `album_id`, `user_id`) VALUES
-(1, '2020-03-01 00:00:00', 9, 1, 31703000),
-(2, '2020-03-02 00:00:00', 10, 1, 31703001),
-(3, '2020-03-03 00:00:00', 8, 2, 31703002),
-(4, '2020-03-04 00:00:00', 9, 2, 31703003),
-(5, '2020-03-05 00:00:00', 8, 3, 31703003);
+INSERT INTO `AlbumRating` (`createtime`, `value`, `album_id`, `user_id`) VALUES
+('2020-03-01 00:00:00', 9, 1, 31703000),
+('2020-03-02 00:00:00', 10, 1, 31703001),
+('2020-03-03 00:00:00', 8, 2, 31703002),
+('2020-03-04 00:00:00', 9, 2, 31703003),
+('2020-03-05 00:00:00', 8, 3, 31703003);
 
 -- --------------------------------------------------------
 
@@ -216,7 +223,9 @@ DROP TABLE IF EXISTS `Direct`;
 CREATE TABLE IF NOT EXISTS `Direct` (
   `movie_id` int NOT NULL,
   `director_id` int NOT NULL,
-  PRIMARY KEY (`movie_id`,`director_id`) USING BTREE
+  PRIMARY KEY (`movie_id`,`director_id`) USING BTREE,
+  INDEX `movie_id_fk` (`movie_id`) USING BTREE,
+  INDEX `director_id_fk` (`director_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
@@ -294,7 +303,6 @@ CREATE TABLE IF NOT EXISTS `Movie` (
   `release_date` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `country` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `detailed_information` text CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-  `director_id` int DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
@@ -302,12 +310,12 @@ CREATE TABLE IF NOT EXISTS `Movie` (
 -- Dumping data for table `Movie`
 --
 
-INSERT INTO `Movie` (`id`, `title`, `release_date`, `country`, `detailed_information`, `director_id`) VALUES
-(1, 'movie_1', '20180505', 'China', 'movie_info_1', 1),
-(2, 'movie_2', '20180502', 'China', 'movie_info_2', 1),
-(3, 'movie_3', '20180503', 'China', 'movie_info_3', 2),
-(4, 'movie_4', '20180504', 'China', 'movie_info_4', 3),
-(5, 'movie_5', '20180505', 'China', 'movie_info_5', 4);
+INSERT INTO `Movie` (`id`, `title`, `release_date`, `country`, `detailed_information`) VALUES
+(1, 'movie_1', '20180505', 'China', 'movie_info_1'),
+(2, 'movie_2', '20180502', 'China', 'movie_info_2'),
+(3, 'movie_3', '20180503', 'China', 'movie_info_3'),
+(4, 'movie_4', '20180504', 'China', 'movie_info_4'),
+(5, 'movie_5', '20180505', 'China', 'movie_info_5');
 
 -- --------------------------------------------------------
 
@@ -322,7 +330,9 @@ CREATE TABLE IF NOT EXISTS `MovieComment` (
   `content` text CHARACTER SET utf8 COLLATE utf8_bin,
   `movie_id` int NOT NULL,
   `user_id` int NOT NULL,
-  PRIMARY KEY (`comment_id`) USING BTREE
+  PRIMARY KEY (`comment_id`) USING BTREE,
+  INDEX `movie_id_fk` (`movie_id`) USING BTREE,
+  INDEX `user_id_fk` (`user_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
@@ -344,24 +354,25 @@ INSERT INTO `MovieComment` (`comment_id`, `createtime`, `content`, `movie_id`, `
 
 DROP TABLE IF EXISTS `MovieRating`;
 CREATE TABLE IF NOT EXISTS `MovieRating` (
-  `rate_id` int NOT NULL,
   `createtime` datetime DEFAULT NULL,
   `value` int DEFAULT NULL,
   `movie_id` int NOT NULL,
   `user_id` int NOT NULL,
-  PRIMARY KEY (`rate_id`) USING BTREE
+  PRIMARY KEY (`movie_id`, `user_id`) USING BTREE,
+  INDEX `movie_id_fk` (`movie_id`) USING BTREE,
+  INDEX `user_id_fk` (`user_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `MovieRating`
 --
 
-INSERT INTO `MovieRating` (`rate_id`, `createtime`, `value`, `movie_id`, `user_id`) VALUES
-(1, '2020-02-01 00:00:00', 7, 1, 31703001),
-(2, '2020-02-01 00:00:00', 6, 2, 31703003),
-(3, '2020-03-01 00:00:00', 8, 3, 31703003),
-(4, '2020-04-01 00:00:00', 5, 4, 31703004),
-(5, '2020-05-01 00:00:00', 10, 5, 31703004);
+INSERT INTO `MovieRating` (`createtime`, `value`, `movie_id`, `user_id`) VALUES
+('2020-02-01 00:00:00', 7, 1, 31703001),
+('2020-02-01 00:00:00', 6, 2, 31703003),
+('2020-03-01 00:00:00', 8, 3, 31703003),
+('2020-04-01 00:00:00', 5, 4, 31703004),
+('2020-05-01 00:00:00', 10, 5, 31703004);
 
 -- --------------------------------------------------------
 
@@ -371,23 +382,25 @@ INSERT INTO `MovieRating` (`rate_id`, `createtime`, `value`, `movie_id`, `user_i
 
 DROP TABLE IF EXISTS `TrackArtist`;
 CREATE TABLE IF NOT EXISTS `TrackArtist` (
-  `track_name` varchar(225) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `track_id` int NOT NULL,
   `album_id` int NOT NULL,
   `artist_id` int NOT NULL,
-  PRIMARY KEY (`track_name`, `album_id`, `artist_id`) USING BTREE
+  PRIMARY KEY (`track_id`, `album_id`, `artist_id`) USING BTREE,
+  INDEX `track_fk` (`track_id`, `album_id`) USING BTREE,
+  INDEX `artist_id_fk` (`artist_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `TrackArtist`
 --
 
-INSERT INTO `TrackArtist` (`track_name`, `album_id`, `artist_id`) VALUES
-('track_1', 1, 1),
-('track_2', 1, 1),
-('track_3', 1, 1),
-('track_4', 1, 1),
-('track_1', 2, 1),
-('track_1', 5, 2);
+INSERT INTO `TrackArtist` (`track_id`, `album_id`, `artist_id`) VALUES
+(1, 1, 1),
+(2, 1, 1),
+(3, 1, 1),
+(4, 1, 1),
+(1, 2, 1),
+(1, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -397,22 +410,24 @@ INSERT INTO `TrackArtist` (`track_name`, `album_id`, `artist_id`) VALUES
 
 DROP TABLE IF EXISTS `Track`;
 CREATE TABLE IF NOT EXISTS `Track` (
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `id` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `album_id` int NOT NULL,
-  PRIMARY KEY (`name`, `album_id`) USING BTREE
+  PRIMARY KEY (`id`, `album_id`) USING BTREE,
+  INDEX `album_id_fk` (`album_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `Track`
 --
 
-INSERT INTO `Track` (`name`, `album_id`) VALUES
-('track_1', 1),
-('track_2', 1),
-('track_3', 1),
-('track_4', 1),
-('track_1', 2),
-('track_1', 5);
+INSERT INTO `Track` (`id`, `name`, `album_id`) VALUES
+(1, 'track_1', 1),
+(2, 'track_2', 1),
+(3, 'track_3', 1),
+(4, 'track_4', 1),
+(1, 'track_1', 2),
+(1, 'track_1', 5);
 
 -- --------------------------------------------------------
 
@@ -422,25 +437,26 @@ INSERT INTO `Track` (`name`, `album_id`) VALUES
 
 DROP TABLE IF EXISTS `TrackRating`;
 CREATE TABLE IF NOT EXISTS `TrackRating` (
-  `rate_id` int NOT NULL,
   `createtime` datetime DEFAULT NULL,
   `value` int DEFAULT NULL,
-  `track_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `track_id` int NOT NULL,
   `album_id` int NOT NULL,
   `user_id` int NOT NULL,
-  PRIMARY KEY (`rate_id`) USING BTREE
+  PRIMARY KEY (`track_id`, `album_id`, `user_id`) USING BTREE,
+  INDEX `track_fk` (`track_id`, `album_id`) USING BTREE,
+  INDEX `user_id_fk` (`user_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `TrackRating`
 --
 
-INSERT INTO `TrackRating` (`rate_id`, `createtime`, `value`, `track_name`, `album_id`, `user_id`) VALUES
-(1, '2020-01-01 00:00:00', 10, 'track_1', 1, 31703001),
-(2, '2020-01-01 00:00:00', 9, 'track_1', 1, 31703001),
-(3, '2020-01-01 00:00:00', 8, 'track_1', 1, 31703001),
-(4, '2020-01-01 00:00:00', 6, 'track_2', 1, 31703001),
-(5, '2020-01-01 00:00:00', 5, 'track_2', 1, 31703001);
+INSERT INTO `TrackRating` (`createtime`, `value`, `track_id`, `album_id`, `user_id`) VALUES
+('2020-01-01 00:00:00', 10, 1, 1, 31703001),
+('2020-01-01 00:00:00', 9, 2, 1, 31703001),
+('2020-01-01 00:00:00', 8, 3, 1, 31703001),
+('2020-01-01 00:00:00', 6, 1, 1, 31703002),
+('2020-01-01 00:00:00', 5, 2, 1, 31703002);
 
 -- --------------------------------------------------------
 
@@ -451,15 +467,13 @@ INSERT INTO `TrackRating` (`rate_id`, `createtime`, `value`, `track_name`, `albu
 DROP TABLE IF EXISTS `User`;
 CREATE TABLE IF NOT EXISTS `User` (
   `id` int NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `music_rating_weight` double DEFAULT NULL,
-  `movie_rating_weight` double DEFAULT NULL,
-  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL UNIQUE,
+  `music_rating_weight` double DEFAULT 10,
+  `movie_rating_weight` double DEFAULT 10,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL UNIQUE,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `certification_information` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE (`email`),
-  UNIQUE (`username`)
+  PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT;
 
 --
@@ -467,76 +481,86 @@ CREATE TABLE IF NOT EXISTS `User` (
 --
 
 INSERT INTO `User` (`id`, `email`, `music_rating_weight`, `movie_rating_weight`, `username`, `password`, `certification_information`) VALUES
-(31703000, '6@sfu.ca', 7, 10, 'user0', '123456', 'Keyboard of Band Re-tros.'),
-(31703001, '7@sfu.ca', 5, 5, 'user1', '123456', NULL),
-(31703002, 'fghf', 5, 5, 'user2', 'fgh', NULL),
-(31703003, 'lkljk', 5, 5, 'user3', 'jjlh', NULL),
-(31703004, 'fghf', 5, 5, 'user4', 'fgh', NULL),
-(31703005, 'lkljk', 5, 5, 'user5', 'jjlh', NULL);
+(31703000, 'a@sfu.ca', 50, 15, 'user0', '123456', 'Keyboard of Band Re-tros.'),
+(31703001, 'b@sfu.ca', 10, 10, 'user1', '123456', NULL),
+(31703002, 'c@sfu.ca', 10, 10, 'user2', 'fgh', NULL),
+(31703003, 'd@sfu.ca', 10, 10, 'user3', 'jjlh', NULL),
+(31703004, 'e@sfu.ca', 10, 10, 'user4', 'fgh', NULL),
+(31703005, 'f@sfu.ca', 10, 10, 'user5', 'jjlh', NULL);
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `act`
+-- Constraints for table `Act`
 --
-ALTER TABLE `act`
-  ADD CONSTRAINT `act_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `act_ibfk_2` FOREIGN KEY (`actor_id`) REFERENCES `actor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Act`
+  ADD CONSTRAINT `act_movie_fk` FOREIGN KEY (`movie_id`) REFERENCES `Movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `act_actor_fk` FOREIGN KEY (`actor_id`) REFERENCES `Actor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `album`
+-- Constraints for table `AlbumArtist`
 --
-ALTER TABLE `album`
-  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`g_id`) REFERENCES `genre` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `album_ibfk_2` FOREIGN KEY (`track_name`) REFERENCES `track` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `album_ibfk_3` FOREIGN KEY (`c_id`) REFERENCES `albumcomment` (`comment_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AlbumArtist`
+  ADD CONSTRAINT `aa_album_fk` FOREIGN KEY (`album_id`) REFERENCES `Album` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `aa_artist_fk` FOREIGN KEY (`artist_id`) REFERENCES `Artist` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `artist`
+-- Constraints for table `AlbumComment`
 --
-ALTER TABLE `artist`
-  ADD CONSTRAINT `artist_ibfk_1` FOREIGN KEY (`g_id`) REFERENCES `genre` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `artist_ibfk_2` FOREIGN KEY (`track_name`) REFERENCES `track` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `artist_ibfk_3` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AlbumComment`
+  ADD CONSTRAINT `ac_album_fk` FOREIGN KEY (`album_id`) REFERENCES `Album` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ac_user_fk` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `direct`
+-- Constraints for table `AlbumRating`
 --
-ALTER TABLE `direct`
-  ADD CONSTRAINT `direct_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `direct_ibfk_2` FOREIGN KEY (`director_id`) REFERENCES `director` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AlbumRating`
+  ADD CONSTRAINT `ar_album_fk` FOREIGN KEY (`album_id`) REFERENCES `Album` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ar_user_fk` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `moviecomment`
+-- Constraints for table `Direct`
 --
-ALTER TABLE `moviecomment`
-  ADD CONSTRAINT `moviecomment_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Direct`
+  ADD CONSTRAINT `direct_movie_fk` FOREIGN KEY (`movie_id`) REFERENCES `Movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `direct_director_fk` FOREIGN KEY (`director_id`) REFERENCES `Director` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `producealbum`
+-- Constraints for table `MovieComment`
 --
-ALTER TABLE `producealbum`
-  ADD CONSTRAINT `producealbum_ibfk_1` FOREIGN KEY (`albumID`) REFERENCES `album` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `producealbum_ibfk_2` FOREIGN KEY (`artistID`) REFERENCES `artist` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `MovieComment`
+  ADD CONSTRAINT `mc_movie_fk` FOREIGN KEY (`movie_id`) REFERENCES `Movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `mc_user_fk` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `producetrack`
+-- Constraints for table `MovieRating`
 --
-ALTER TABLE `producetrack`
-  ADD CONSTRAINT `producetrack_ibfk_1` FOREIGN KEY (`name`) REFERENCES `track` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `producetrack_ibfk_2` FOREIGN KEY (`artistID`) REFERENCES `artist` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `MovieRating`
+  ADD CONSTRAINT `mr_movie_fk` FOREIGN KEY (`movie_id`) REFERENCES `Movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `mr_user_fk` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `user`
+-- Constraints for table `TrackArtist`
 --
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`album_r_id`) REFERENCES `albumrating` (`rate_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`album_c_id`) REFERENCES `albumcomment` (`comment_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_ibfk_3` FOREIGN KEY (`track_r_id`) REFERENCES `trackrating` (`rate_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_ibfk_4` FOREIGN KEY (`movie_c_id`) REFERENCES `moviecomment` (`comment_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_ibfk_5` FOREIGN KEY (`movie_r_id`) REFERENCES `albumrating` (`rate_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `TrackArtist`
+  ADD CONSTRAINT `ta_track_fk` FOREIGN KEY (`track_id`, `album_id`) REFERENCES `Track` (`id`, `album_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ta_artist_fk` FOREIGN KEY (`artist_id`) REFERENCES `Artist` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Track`
+--
+ALTER TABLE `Track`
+  ADD CONSTRAINT `track_album_fk` FOREIGN KEY (`album_id`) REFERENCES `Album` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `TrackRating`
+--
+ALTER TABLE `TrackRating`
+  ADD CONSTRAINT `tr_track_fk` FOREIGN KEY (`track_id`, `album_id`) REFERENCES `Track` (`id`, `album_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tr_user_fk` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
